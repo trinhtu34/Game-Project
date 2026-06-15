4c8df40261f5ef843b4a25e424d46542e8a07be6
bstHoang <10t1qx4thaihoang@gmail.com>
2026-06-15 09:38:14 +0700

    upload base

---
+++ MageProject/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator anim;
+    private Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Chỉ cập nhật hướng nhìn khi người chơi có bấm nút (để giữ nguyên hướng khi đứng im)
+        if (movement != Vector2.zero)
+        {
+            anim.SetFloat("Horizontal", movement.x);
+            anim.SetFloat("Vertical", movement.y);
+        }
+
+        // Truyền vận tốc vào Animator để quyết định trạng thái Đi hay Đứng
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
